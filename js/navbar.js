b2ce3b535b4c8fe611432a8209f9d7674053adc3
Raphael <raphaelarzoin173@gmail.com>
2024-05-07 22:23:35 +0200

    100%

--- js/navbar.js
+++ js/navbar.js
@@ -7,12 +7,8 @@ function scrollFunction() {
         document.getElementById("navbar").style.display = "none";
     }
 }
-function showOverlay() {
-    document.getElementById("overlay").classList.remove("hidden");
-    document.body.classList.add("no-scroll");
-}
-
-function hideOverlay() {
-    document.getElementById("overlay").classList.add("hidden");
-    document.body.classList.remove("no-scroll");
-}
+// Capturer l'événement de rafraîchissement de la page
+window.onbeforeunload = function() {
+    // Faire défiler vers la première div
+    document.getElementById('premierPartie').scrollIntoView();
+};
